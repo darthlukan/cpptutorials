f4809f7ee6f3050b60ddb9dfbc67fe068ece79e9
Brian Tomlinson <darthlukan@gmail.com>
2013-07-05 23:13:49 +0200

    a first look at how header files work. add.h declares what add.cpp provides and main.cpp includes add.h to have access to what is inside of add.cpp.

---
+++ headers/add.h
@@ -0,0 +1,6 @@
+#ifndef ADD_H
+#define ADD_H
+
+int add(int x, inty); // Function prototype for add.h
+
+#endif
---
+++ headers/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "add.h"
+
+int main() {
+    using namespace std;
+
+    cout << "The sum of 3 and 4 is " << add(3, 4) << endl;
+
+    return 0;
+}
